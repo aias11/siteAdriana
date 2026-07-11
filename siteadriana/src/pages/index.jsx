6714cf2c1fbad2202ad6ab990e938dd120b5f2a0
Vinicius Aias <viniciusaias200@gmail.com>
2026-07-11 14:36:09 -0300

    começo do projeto

---
+++ siteadriana/src/pages/index.jsx
@@ -0,0 +1,14 @@
+import react from 'react';
+import '../styles/index.css';
+
+function Index() {
+    return (
+        <section>
+            <div>
+                <h1>Teste</h1>
+            </div>
+        </section>
+    )
+}
+
+export default Index;
